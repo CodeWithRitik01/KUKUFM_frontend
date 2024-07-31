9fb7629d9df14ceda43302181f14dbe127964746
CodeWithRitik01 <rkritik.9958@gmail.com>
2024-07-31 12:19:20 +0530

    final

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -7,12 +7,13 @@ import { NavLink } from "react-router-dom";
 function Navbar(){
     const languages = ["Hindi", "Marathi", "Bangla", "Telugu", "Tamil", "Malayalam", "Kannada","English"]
     const [isOverlayOpen, setIsOverlayOpen] = useState(false);
-    return(
+    return( 
         <div className={styles.main}>
             <LazyImageLoader onClick={() => setIsOverlayOpen(true)} className={`${styles.menu} ${styles.otherNav}`} src="https://cdn-icons-png.flaticon.com/128/4204/4204600.png" alt="not found"/>
             <Sidebar isOpen={isOverlayOpen} onClose={() => setIsOverlayOpen(false)}>
              <h1>This is an overlay content</h1>
             </Sidebar>
+            
             <NavLink>
               <LazyImageLoader className={`${styles.logo} ${styles.otherNav}`} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfzm77sALhnUKUt6fD6P8q1kQqvOxykOxfH6FogWxm4HpY_e_Ccrogwd2kFGKNhVM29no&usqp=CAU" alt="not found"/>
             </NavLink>
